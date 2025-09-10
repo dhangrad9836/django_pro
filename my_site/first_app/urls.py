42c57a6516a2a3d87efbece184e15919832ab18c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-09-10 00:01:37 -0400

    added templates folder at project level, created a example.html file which will be rendered in our simple_view func in views.py, urls.py points the loc of the simple_view to render the example.html page

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -2,17 +2,7 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path("sports", views.sports_view),
-#     path("finance", views.finance_view),
-# ]  # this is the url path we use to access the app on the browser)]
-
-
-# so we will now use the news_view function to dynamically render views based on the articles dictionary
-# also we used a path converter of str:topic
-# <str:topic> to capture the topic from the url and pass it to the news_view function as a parameter
 urlpatterns = [
-    path("<int:num_page>", views.num_page_view),
-    path("<str:topic>", views.news_view, name="topic-page"),
-    # path("<int:num1>/<int:num2>", views.add_view),
-]  # this is the url path we use to access the app on the browser)]
+    path('', views.simple_view) # we need to find a function called simple_view in views.py...domain.com/first_app/
+    
+]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -8,59 +8,6 @@ from django.http.response import (
 from django.urls import reverse
 
 # Create your views here.
-
-
-# def sports_view(request):
-#     return HttpResponse("Sports Page")
-
-
-# def finance_view(request):
-#     return HttpResponse("Finance Page")
-
-# so above we used function views for each type of view which can get tedious if we have many views
-# so for now we will dynamically render views using a articles dictionary...we will define below the articles dictionary a single function
-
-articles = {
-    "sports": "Sports Page",
-    "finance": "Finance Page",
-    "politics": "Politics Page",
-}
-
-
-# our function will be called news_view and will take in a request and topic parameter, whichh is the value in the dictionary and make sure to import Http404
-def news_view(request, topic):
-    # we will check if the topic is in the articles dictionary
-    # we use a try/except and in the except we will return a 404 error using HttpResponseNotFound
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        # result = "No page for that topic"
-        # return HttpResponseNotFound(result)
-        raise Http404("404 GENEREIC ERROR")
-
-
-# here we will create a view that takes in a number from the url and redirects to the corresponding topic page
-# domain.com/first_app/0 will redirect to domain.com/first_app/sports
-# domain.com/first_app/1 will redirect to domain.com/first_app/finance
-
-
-# make sure to import HttpResponseRedirect above
-def num_page_view(request, num_page):
-
-    topics_list = list(articles.keys())  # ['sports', 'finance', 'politics']
-    topic = topics_list[
-        num_page
-    ]  # this will get the topic based on the index passed in the url
-
-    # webpage = reverse("topic-page", args=[topic])
-    return HttpResponseRedirect(
-        reverse("topic-page", args=[topic])
-    )  # this will redirect to the topic page
-
-
-# make sure to import HttpResponse above
-def add_view(request, num1, num2):
-    add_result = num1 + num2
-    result = f"{num1} + {num2} = {add_result}"
-    return HttpResponse(str(result))
+#the simple_view takes in a request and returns a response
+def simple_view(request):
+    return render(request, 'first_app/example.html') #theres some html file in my directories..so where should i point it to?
