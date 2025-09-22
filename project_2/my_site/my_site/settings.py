f3341436eea1beb46ed5abe611350767a42b13c5
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-09-22 13:49:25 -0400

    added custom static files

--- project_2/my_site/my_site/settings.py
+++ project_2/my_site/my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6&mobfq$=*v0%zbd=s@2v2#r$hziu56t!x!c$t=1shyfs*-v+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
