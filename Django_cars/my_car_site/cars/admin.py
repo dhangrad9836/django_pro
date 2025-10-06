9946bf033e3d4de606b4268d0c4bb2bf8089b84c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-10-06 12:54:54 -0400

    updated admin.py and included fieldsets

--- Django_cars/my_car_site/cars/admin.py
+++ Django_cars/my_car_site/cars/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+#admin.site.register(Car)
+
+#https://docs.djangoproject.com/en/5.2/ref/contrib/admin/# ModelAdmin/
+# the convention name is modelname and Admin next to it...so here it's CarAdmin and we are extending admin.ModelAdmin
+class CarAdmin(admin.ModelAdmin):
+    #here we can customize the admin interface for the Car model..we can change the order of the fields
+    #fields = ['year', 'brand']
+    fieldsets = [
+        ('TIME INFORMATION', {'fields': ['year']}), # this is a tuple...the first element is the title of the fieldset...the second element is a dictionary with a fields key and a list of fields as the value
+        ('CAR INFORMATION', {'fields': ['brand']}),
+    ]
+
+# so you register the model Car with the CarAdmin class
+admin.site.register(Car, CarAdmin)
